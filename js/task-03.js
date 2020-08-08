b3836398f457206b0a77a256c073d7e6d4795256
Yevhenii Korkots <atc0884@gmail.com>
2020-08-08 23:13:40 +0300

    complete

--- js/task-03.js
+++ js/task-03.js
@@ -1,36 +1,36 @@
-// "use strict";
+"use strict";
 
-// class Storage {
-//   constructor(items) {
-//     this.items = items;
-//   }
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
 
-//   getItems() {
-//     return this.items;
-//   }
-//   addItem(item) {
-//     return this.items.push(item);
-//   }
-//   removeItem(item) {
-//     if (this.items.indexOf(item)) {
-//       this.items.splice(this.items.indexOf(item), 1);
-//     }
-//     return this.items;
-//   }
-// }
+  getItems() {
+    return this.items;
+  }
+  addItem(item) {
+    return this.items.push(item);
+  }
+  removeItem(item) {
+    if (this.items.indexOf(item)) {
+      this.items.splice(this.items.indexOf(item), 1);
+    }
+    return this.items;
+  }
+}
 
-// const storage = new Storage([
-//   "Нанитоиды",
-//   "Пролонгер",
-//   "Железные жупи",
-//   "Антигравитатор",
-// ]);
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор",
+]);
 
-// const items = storage.getItems();
-// console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
 
-// storage.addItem("Дроид");
-// console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+storage.addItem("Дроид");
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
 
-// storage.removeItem("Пролонгер");
-// console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
--- js/task-04.js
+++ js/task-04.js
@@ -1,32 +1,32 @@
-// "use strict";
+"use strict";
 
-// class StringBuilder {
-//   constructor(value) {
-//     this._value = value;
-//   }
+class StringBuilder {
+  constructor(value) {
+    this._value = value;
+  }
 
-//   get value() {
-//     return this._value;
-//   }
+  get value() {
+    return this._value;
+  }
 
-//   append(str) {
-//     this._value += str;
-//   }
-//   prepend(str) {
-//     this._value = str + this._value;
-//   }
-//   pad(str) {
-//     this._value = str + this._value + str;
-//   }
-// }
+  append(str) {
+    this._value += str;
+  }
+  prepend(str) {
+    this._value = str + this._value;
+  }
+  pad(str) {
+    this._value = str + this._value + str;
+  }
+}
 
-// const builder = new StringBuilder(".");
+const builder = new StringBuilder(".");
 
-// builder.append("^");
-// console.log(builder.value); // '.^'
+builder.append("^");
+console.log(builder.value); // '.^'
 
-// builder.prepend("^");
-// console.log(builder.value); // '^.^'
+builder.prepend("^");
+console.log(builder.value); // '^.^'
 
-// builder.pad("=");
-// console.log(builder.value); // '=^.^='
+builder.pad("=");
+console.log(builder.value); // '=^.^='
